1593fc1f38eb3dba5a9c937a4da875e0d0e00e90
Asekenye Rachael <asekenyerachael037@gmail.com>
2018-11-18 21:53:16 +0300

    updated work

---
+++ power.py
@@ -0,0 +1,17 @@
+def power(a,b):
+    if (b == 0):
+        return 1
+    elif a == 1:
+            return 1
+
+            
+    for i in range(1:int):
+        a*power(a,b-1)
+        return product
+
+    
+
+if __name__=='__main__':
+
+    print(power(2,4))
+
--- sort.py
+++ sort.py
@@ -1,26 +1,35 @@
 def mylist(sorted_list):
-    sorted_list = [2,0,3,6,5,9,4,7,'s','n']
-    list_of_odds = []
-    list_of_evens = []
-    list_of_chars = []
-    list_of_others = []
-    new_dict = {}
+
+    
+
+    odds = []
+    evens = []
+    characters = []
+
+    new_dict = dict()
+    
+    new_dict['odds'] = odds
+    new_dict['evens'] = evens
+    new_dict['chars'] = characters
+
+   
+
     for x in sorted_list:
         if isinstance(x,int):
-            if(x%  2 == 0):
-                list_of_evens.append(x)
+            if x %  2 == 0:
+               evens.append(x)
             else:
-                list_of_odds.append(x)    
-        elif isinstance(x,str):
-             list_of_chars.append(x)
-        else:
-             list_of_others.append(x)
-    new_dict['odds'] = sorted(list_of_odds)
-    new_dict['evens'] = sorted(list_of_evens)
-    new_dict['chars'] = sorted(list_of_chars)
-    new_dict ['others'] = sorted(list_of_others)
+              odds.append(x)    
+        elif isinstance(x, str):
+            characters.append(x)
+    
+    new_dict['odds'] = sorted(odds)
+    new_dict['evens'] = sorted(evens)
+    new_dict['chars'] = sorted(characters)
+    return new_dict
 
-    print(sorted_list)
+if __name__=='__main__':
+   print (mylist([2,0,3,6,5,9,4,7,'s','n']))
 
 
 
